d6e8b99d1ce93745ef2926d1649450e63d5951fe
roberta-coloradogirl <roberta@coloradogirl.org>
2014-05-10 14:35:05 -0600

    Accept todo input
    Currently a problem displaying the added todo item.

--- toDoMVC.js
+++ toDoMVC.js
@@ -1,28 +1,47 @@
 var ENTER_KEY = 13;
 var ESCAPE_KEY = 27;
+var listCount = 0;
+var completedCount = 0;
 
 $(document).ready(function() {
    var App = {
       init: function() {
-
+         $("#new-todo").focus;
       },
    };
+
+   function addtoList(item) {
+      console.log("enter addtoList");
+      console.log("   do: "+item);
+      $("#listTemplate>li").clone().append("#theList");
+/*      $(".listTemplate>li>span.itemCheckOff").clone().add(newLi);
+      $(".listTemplate>li>span.listItem").clone().add(newLi);
+      $(".listTemplate>li>span.removeItem").clone().add(newLi);
+      console.log(newLi);
+      newLi.append("theList");      */
+      listCount += 1;
+      $("#listCount").text(listCount);
+   };
+
    function setListeners() {
       $("#new-todo").on("keypress",function(evt) {
 //         console.log("key selected: "+evt.which);
          if (evt.which == ENTER_KEY) {
-            console.log("clicked enter_key");
+            console.log("clicked enter_key in #new-todo");
+            addtoList($("input:text").val());
+            $("input:text").val("");
          } else if (evt.which == ESCAPE_KEY) {
-            console.log("clicked escape_key");
+            console.log("clicked escape_key in #new-todo");
          } else {
             console.log("clicked some key");
          };
       });
       $("#listChevron").on("hover",function() {
-         console.log("listChevron hover");
+         console.log("listChevron hover in #listChevron");
+         this.background("silver");
       });
       $("#listChevron").on("click",function() {
-         console.log("listChevron click");
+         console.log("listChevron click in listChevron");
       });
       $("listEditable").on("",function() {});
       $("theListDisplay").on("",function() {});
@@ -47,6 +66,9 @@ $(document).ready(function() {
       $("removeCompleted").on("click",function() {
          console.log("removeCompleted click");
       });
+
+      
    };
    setListeners();
+   App.init();
 });
